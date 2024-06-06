a297934665be45c7cb4029b4df0f97ae3b84c92c
nasriyasoftware <nasriya.software@outlook.com>
2024-06-06 11:34:36 +0300

    Fixed tests

---
+++ tests/uuidX.test.ts
@@ -0,0 +1,20 @@
+import uuidX from '../src/uuidX';
+
+describe('Testing UUID', () => {
+    const v4 = uuidX.v4();
+
+    describe('Check v4', () => {
+        test('Generated type is string', () => {
+            expect(typeof v4).toBe('string');
+        })
+
+        test('Expected 4 dahses', () => {
+            expect(v4.split('-').length).toBe(5);
+        })
+
+        test('Expect a version 4 code', () => {
+            const parts = v4.split('-');
+            expect(parts[2].startsWith('4'));
+        })
+    })
+})
